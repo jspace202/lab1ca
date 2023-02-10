e1ff2ac0bc0b065fc39743cebfe8c7caa553993f
madssteele <madelesteele@gmail.com>
2023-02-09 18:51:26 -0600

    Update sim.c

--- lab1/src/sim.c
+++ lab1/src/sim.c
@@ -92,51 +92,51 @@ int r_process(char* i_) {
     ADD(Rd, Rs1, Rs2, Funct3);
     return 0;
   }
-  /*if(!strcmp(d_opcode,"0110011")) {
+  else if(!strcmp(d_opcode,"0110011")) {
     printf("--- This is an SRL instruction. \n");
     SRL(Rd);
     return 0;
   }
-  if(!strcmp(d_opcode,"0110011")) {
+  else if(!strcmp(d_opcode,"0110011")) {
     printf("--- This is an SUB instruction. \n");
     SUB(Rd, Rs1, Rs2, Funct3);
     return 0;
-  }*/
-  if(!strcmp(d_opcode,"0110011")) {
+  }
+  else if(!strcmp(d_opcode,"0110011")) {
     printf("--- This is an OR instruction. \n");
     OR(Rd, Rs1, Rs2, Funct3);
     return 0;
-  }/*
-  if(!strcmp(d_opcode,"0110011")) {
+  }
+  else if(!strcmp(d_opcode,"0110011")) {
     printf("--- This is an XOR instruction. \n");
     XOR(Rd, Rs1, Rs2, Funct3);
     return 0;
   }
-  if(!strcmp(d_opcode,"0110011")) {
+  else if(!strcmp(d_opcode,"0110011")) {
     printf("--- This is an AND instruction. \n");
     AND(Rd, Rs1, Rs2, Funct3);
     return 0;
   }
-  if(!strcmp(d_opcode,"0110011")) {
+  else if(!strcmp(d_opcode,"0110011")) {
     printf("--- This is an SRA instruction. \n");
     SRA(Rd, Rs1, Rs2, Funct3);
     return 0;
   }
-  if(!strcmp(d_opcode,"0110011")) {
+  else if(!strcmp(d_opcode,"0110011")) {
     printf("--- This is an SLT instruction. \n");
     SLT(Rd, Rs1, Rs2, Funct3);
     return 0;
   }
-  if(!strcmp(d_opcode,"0110011")) {
+  else if(!strcmp(d_opcode,"0110011")) {
     printf("--- This is an SLTU instruction. \n");
     SLTU(Rd, Rs1, Rs2, Funct3);
     return 0;
   }
-  if(!strcmp(d_opcode,"0110011")) {
+  else if(!strcmp(d_opcode,"0110011")) {
     printf("--- This is an SLL instruction. \n");
     SLL(Rd, Rs1, Rs2, Funct3);
     return 0;
-  }*/
+  }
   /* Add other data instructions here */ 
 
   return 1;	
